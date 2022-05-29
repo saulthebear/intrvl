ef921662e0b70625f1fd04ae2d3a4b56b3ebc52d
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-29 13:25:57 -0600

    👌 IMPROVE: Add Dev & Test log files w/ timestamps

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,8 +1,13 @@
 const winston = require("winston")
 
+const env = process.env.NODE_ENV
+
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
     new winston.transports.File({ filename: "logs/combined.log" }),
     new winston.transports.File({
@@ -12,7 +17,7 @@ const logger = winston.createLogger({
   ],
 })
 
-const env = process.env.NODE_ENV
+// Dev environment:
 if (!(env === "production" || env === "test")) {
   logger.add(
     new winston.transports.Console({
@@ -21,6 +26,30 @@ if (!(env === "production" || env === "test")) {
         winston.format.colorize(),
         winston.format.simple()
       ),
+    })
+  )
+  logger.add(
+    new winston.transports.File({
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.simple()
+      ),
+      filename: "logs/dev.log",
+    })
+  )
+}
+
+// Output logs to file in test env
+if (env === "test") {
+  logger.add(
+    new winston.transports.File({
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.simple(),
+        winston.format.timestamp()
+      ),
+      filename: "logs/test.log",
     })
   )
 }
