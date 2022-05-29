e6676727f8855a9419de3c5abb4ea71f3466777a
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-28 20:18:19 -0600

    📦 NEW: Add login helper methods to user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 "use strict"
 const { Model } = require("sequelize")
 const bcrypt = require("bcryptjs")
+const cryptoJS = require("crypto-js")
+const logger = require("../helpers/logger")
+
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -17,8 +20,41 @@ module.exports = (sequelize, DataTypes) => {
       return bcrypt.hashSync(password, 12)
     }
 
+    static findByEncryptedId(encryptedId) {
+      let foundUser
+      try {
+        const decryptedId = cryptoJS.AES.decrypt(
+          encryptedId,
+          process.env.ENC_KEY
+        ).toString(cryptoJS.enc.Utf8)
+
+        if (!decryptedId) throw "Invalid encryptedId"
+
+        foundUser = user.findByPk(decryptedId)
+      } catch (error) {
+        logger.debug("Could not find user by encryptedId")
+        return null
+      }
+
+      return foundUser
+    }
+
     verifyPassword(password) {
       return bcrypt.compareSync(password, this.passwordHash)
+    }
+
+    getEncryptedId() {
+      const stringId = this.id.toString()
+      const encryptedId = cryptoJS.AES.encrypt(stringId, process.env.ENC_KEY)
+      return encryptedId.toString()
+    }
+
+    verifyEncryptedId(encryptedId) {
+      const decryptedId = cryptoJS.AES.decrypt(
+        encryptedId,
+        process.env.ENC_KEY
+      ).toString(cryptoJS.enc.Utf8)
+      return decryptedId == this.id
     }
   }
   user.init(
--- test/users/users.test.js
+++ test/users/users.test.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
 const chai = require("chai")
 const bcrypt = require("bcryptjs")
 const request = require("supertest")
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const cryptoJS = require("crypto-js")
 
 const db = require("../../models")
 const clearDb = require("../clearDb")
@@ -35,7 +37,7 @@ describe("Users model", () => {
     })
   })
 
-  describe("user#hashPassword", () => {
+  describe("user.hashPassword", () => {
     let username, password
     before(async () => {
       const data = await userData()
@@ -81,6 +83,94 @@ describe("Users model", () => {
       user.verifyPassword(password).should.equal(true)
     })
   })
+
+  describe("user#getEncryptedId", () => {
+    let user
+    before(async () => {
+      const data = await userData()
+
+      const passwordHash = bcrypt.hashSync(data.password, 12)
+      user = await db.user.create({ username: data.username, passwordHash })
+    })
+
+    it("exists", () => {
+      user.getEncryptedId.should.be.a("function")
+    })
+
+    it("returns a string", () => {
+      const encryptedId = user.getEncryptedId()
+      encryptedId.should.be.a("string")
+    })
+
+    it("decrypts to matching id", () => {
+      const encryptedId = user.getEncryptedId()
+      const decryptedId = cryptoJS.AES.decrypt(
+        encryptedId,
+        process.env.ENC_KEY
+      ).toString(cryptoJS.enc.Utf8)
+
+      decryptedId.should.be.equal(user.id.toString())
+    })
+  })
+
+  describe("user#verifyEncryptedId", () => {
+    let user
+    before(async () => {
+      const data = await userData()
+
+      const passwordHash = bcrypt.hashSync(data.password, 12)
+      user = await db.user.create({ username: data.username, passwordHash })
+    })
+
+    it("exists", () => {
+      user.verifyEncryptedId.should.be.a("function")
+    })
+
+    it("returns a boolean", () => {
+      user.verifyEncryptedId("string").should.be.a("boolean")
+    })
+    it("identifies correct match", () => {
+      const encryptedId = cryptoJS.AES.encrypt(
+        user.id.toString(),
+        process.env.ENC_KEY
+      )
+
+      user.verifyEncryptedId(encryptedId).should.equal(true)
+    })
+    it("identifies incorrect match", () => {
+      user.verifyEncryptedId(user.id.toString()).should.equal(false)
+    })
+  })
+
+  describe("user.findByEncryptedId", () => {
+    let user
+    before(async () => {
+      const data = await userData()
+
+      const passwordHash = bcrypt.hashSync(data.password, 12)
+      user = await db.user.create({ username: data.username, passwordHash })
+    })
+    it("exists", () => {
+      db.user.findByEncryptedId.should.be.a("function")
+    })
+    it("returns a user with valid encrypted id", async () => {
+      const id = user.id
+      const encryptedId = cryptoJS.AES.encrypt(
+        user.id.toString(),
+        process.env.ENC_KEY
+      )
+
+      const foundUser = await db.user.findByEncryptedId(encryptedId)
+      foundUser.should.be.an.instanceof(db.user)
+      foundUser.id.should.be.equal(user.id)
+    })
+    it("returns null with invalid encrypted id", async () => {
+      const foundUser = await db.user.findByEncryptedId(
+        "U2FsdGVkX19OC0nwb96Nh22oeoMnJk3uXOEcFGPO8bg%3D"
+      )
+      should.not.exist(foundUser)
+    })
+  })
 })
 
 describe("user routes", () => {
@@ -96,6 +186,32 @@ describe("user routes", () => {
   describe("POST /users/login", () => {
     describe("Valid credentials", () => {
       it("sets a cookie")
+      // let validUsername, validPassword
+      // before(async () => {
+      //   // Create a user
+      //   const data = await userData()
+
+      //   validUsername = data.username
+      //   validPassword = data.password
+
+      //   // console.log("Username >>>", validUsername)
+      //   // console.log("Password >>>", validPassword)
+
+      //   const passwordHash = db.user.hashPassword(validPassword)
+      //   // console.log("Hash>>>", passwordHash)
+      //   await db.user.create({
+      //     username: validUsername,
+      //     passwordHash: passwordHash,
+      //   })
+      // })
+
+      // it("sets a cookie", (done) => {
+      //   request(app)
+      //     .post("/users/login")
+      //     .send(`username=${validUsername}, password=${validPassword}`)
+      //     .expect((response) => console.log("HEADERS>>>", response.headers))
+      //     .expect(200, done)
+      // })
 
       it("encrypts user id")
 
@@ -139,7 +255,16 @@ describe("user routes", () => {
         const user = await db.user.findOne({ where: { username } })
         user.should.be.an.instanceof(db.user)
       })
-      it("hashes the user's password")
+      it("hashes the user's password", async () => {
+        await request(app)
+          .post("/users")
+          .send(`username=${username}`)
+          .send(`password=${password}`)
+          .expect(201)
+
+        const user = await db.user.findOne({ where: { username } })
+        user.passwordHash.should.not.be.equal(password)
+      })
       it("logs the user in")
     })
 
