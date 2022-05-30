097f922a6221e2db4808a82f1f24bd4ebcd7b02b
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-30 12:28:40 -0600

    📦 NEW: Implement timer routes and views

--- controllers/timers.js
+++ controllers/timers.js
@@ -1,48 +1,133 @@
 const express = require("express")
 const db = require("../models")
+const logger = require("../helpers/logger")
+const chalk = require("chalk")
 
 const router = express.Router()
 
 // ANCHOR new timer form
 // GET /timers/new
-router.get("/new", (req, res) => {
-  res.render("timers/new")
+router.get("/new", async (req, res) => {
+  const messages = await req.flash("message")
+  res.render("timers/new", { messages, timer: null })
 })
 
-// STUB create timer
+// ANCHOR create timer
 // POST /timers
-router.post("/", (req, res) => {
-  res.send("should create a timer")
+router.post("/", async (req, res) => {
+  try {
+    const name = req.body.name
+    const duration = parseInt(req.body.duration)
+    const repeat = !!req.body.repeat
+
+    const user = res.locals.user
+    const timer = await user.createTimer({ name, duration, repeat })
+
+    if (timer) {
+      logger.debug(chalk.green(`⏲ Timer created! id: ${timer.id}`))
+      res.redirect(`/timers/${timer.id}`)
+    } else {
+      req.flash("message", "Unable to create timer. Try again.")
+      res.redirect("/timer/new")
+    }
+  } catch (error) {
+    logger.error(chalk.red("Error creating timer!: "), error)
+    res.status(500)
+    res.render("500")
+  }
 })
 
-// STUB show a timer
+// ANCHOR show a timer
 // GET /timers/:id
-router.get("/:id", (req, res) => {
-  res.render("timers/show")
+router.get("/:id", async (req, res) => {
+  try {
+    const timer = await db.Timer.findByPk(req.params.id)
+    res.render("timers/show", { timer })
+  } catch (error) {
+    logger.error(chalk.red("Error in timer show page! "), error)
+    res.status(500)
+    res.render("500")
+  }
 })
 
-// STUB update timer
-// PUT /timers/:id
-router.put("/:id", (req, res) => {
-  res.send("should update timer")
-})
-
-// STUB delete timer
-// DELETE /timers/:id
-router.delete("/:id", (req, res) => {
-  res.send("should delete timer")
-})
-
-// STUB edit timer form
+// ANCHOR edit timer form
 // GET /timers/:id/edit
-router.get("/:id/edit", (req, res) => {
-  res.render("timers/edit")
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const timer = await db.Timer.findByPk(req.params.id)
+
+    if (!timer) {
+      res.status(404)
+      res.render("404")
+      return
+    }
+
+    const messages = await req.flash("message")
+    res.render("timers/edit", { messages, timer })
+  } catch (error) {
+    logger.error(chalk.red("Error showing timer edit form: "), error)
+    res.status(500)
+    res.render("500")
+  }
+})
+
+// ANCHOR update timer
+// PUT /timers/:id
+router.put("/:id", async (req, res) => {
+  try {
+    const timer = await db.Timer.findByPk(req.params.id)
+
+    if (!timer) throw "Could not find timer!"
+
+    const name = req.body.name
+    const duration = parseInt(req.body.duration)
+    const repeat = !!req.body.repeat
+
+    await timer.update({ name, duration, repeat })
+    await timer.save()
+
+    res.redirect(`/timers/${req.params.id}`)
+  } catch (error) {
+    logger.error(chalk.red("Error updating timer: "), error)
+    res.status(500)
+    res.render("500")
+  }
+})
+
+// ANCHOR delete timer
+// DELETE /timers/:id
+router.delete("/:id", async (req, res) => {
+  try {
+    const timer = await db.Timer.findByPk(req.params.id)
+
+    if (!timer) {
+      res.status(404)
+      res.render("404")
+      return
+    }
+
+    await timer.destroy()
+
+    req.flash("message", "timer deleted")
+    res.redirect(`/users/${res.locals.user.id}`)
+  } catch (error) {
+    logger.error(chalk.red("Error deleting timer: "), error)
+    res.status(500)
+    res.render("500")
+  }
 })
 
 // STUB timers index
 // GET /timers
-router.get("/", (req, res) => {
-  res.send("should show all timers")
+router.get("/", async (req, res) => {
+  try {
+    const timers = await db.Timer.findAll()
+    res.render("timers/index", { timers })
+  } catch (error) {
+    logger.error(chalk.red("Error showing all timers: "), error)
+    res.status(500)
+    res.render("500")
+  }
 })
 
 module.exports = router
