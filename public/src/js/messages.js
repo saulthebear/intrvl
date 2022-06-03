99db34321c6e3084927ab1c8b311b73d2d5d7ddf
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-03 13:59:56 -0600

    👌 IMPROVE: Auto dismiss flash messages
    * Error messages are dismissed after 5 seconds
    * other messages are dismissed after 1.5 seconds

---
+++ public/src/js/messages.js
@@ -0,0 +1,27 @@
+// All flash messages
+// const flashElements = Array.from(document.querySelectorAll(".js-flash"))
+
+// Specific flash message types
+const flashErrors = Array.from(document.querySelectorAll(".js-flash-error"))
+const flashMessages = Array.from(document.querySelectorAll(".js-flash-error"))
+const flashInfos = Array.from(document.querySelectorAll(".js-flash-info"))
+const flashSuccess = Array.from(document.querySelectorAll(".js-flash-success"))
+
+const options = { duration: 2000 }
+
+// const flashDismissObjects = flashElements.map(
+//   (element) => new Dismiss(element, options)
+// )
+
+const errorDismissObjects = flashErrors.map(
+  (element) => new Dismiss(element, options)
+)
+
+const otherMessages = flashMessages.concat(flashInfos, flashSuccess)
+const otherDismissObjects = otherMessages.map(
+  (element) => new Dismiss(element, options)
+)
+
+// flashDismissObjects.forEach((dismiss) => setTimeout(() => dismiss.hide(), 2000))
+errorDismissObjects.forEach((dismiss) => setTimeout(() => dismiss.hide(), 5000))
+otherDismissObjects.forEach((dismiss) => setTimeout(() => dismiss.hide(), 1500))
