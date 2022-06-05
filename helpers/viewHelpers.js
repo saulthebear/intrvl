3d3e877ab2ed919653c62a071f778afc3f20329f
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-05 17:05:05 -0600

    📦 NEW: Add dark mode

--- helpers/viewHelpers.js
+++ helpers/viewHelpers.js
@@ -1,11 +1,11 @@
 const setViewHelpers = (req, res, next) => {
   res.locals.colors = {
-    red: "bg-red-200",
-    orange: "bg-orange-200",
-    yellow: "bg-yellow-200",
-    green: "bg-green-200",
-    blue: "bg-blue-200",
-    purple: "bg-purple-200",
+    red: "bg-red-200 dark:bg-red-700",
+    orange: "bg-orange-200 dark:bg-orange-700",
+    yellow: "bg-yellow-200 dark:bg-yellow-700",
+    green: "bg-green-200 dark:bg-green-700",
+    blue: "bg-blue-200 dark:bg-blue-700",
+    purple: "bg-purple-200 dark:bg-purple-700",
   }
   next()
 }
