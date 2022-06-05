3a58e1e94ce2ce97bd248a0b0620b5629d2d1a2f
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-05 12:04:39 -0600

    📦 NEW: Add typing animation to index page

---
+++ public/src/js/index.js
@@ -0,0 +1,11 @@
+// Logic for typing animation on index page
+const typeOptions = {
+  stringsElement: "#typed-strings",
+  typeSpeed: 50,
+  loop: false,
+  showCursor: false,
+  backDelay: 850,
+  backSpeed: 30,
+}
+
+const typed = new Typed("#js-animated-type", typeOptions)
